ca5c6082eeef77f42220ab12102689f7161028dc
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-05-26 02:25:32 +0700

    check case

--- main.py
+++ main.py
@@ -14,17 +14,55 @@ def getCommand(file):
 
 
 def checkCase(command):
-    cmd = []
-    for i in command:
-        cmd.append(i)  # String -list
-        match cmd[i]:
-            case "create_hotel":
-                print("create hotel")
-            
-
-       
+    # print(len(command))
+    i = 0
+    while i < len(command):
+        cmd = command[i]
         
+        if cmd == "create_hotel":
+            print("create_hotel")
+            return
+           
+        elif cmd == "book":
+            print("book")
+            return
+            
+        elif cmd == "list_available_rooms":
+            print("list_available_rooms")
+            return
+           
+        elif cmd == "checkout":
+            print("checkout")
+            return
+            
+        elif cmd == "list_guest":
+            print("list_guest")
+            return
+        
+        elif cmd == "get_guest_in_room":
+            print("get_guest_in_room")
+            return
 
+        elif cmd == "list_guest_by_age":
+            print("list_guest_by_age ")
+            return
+        
+        elif cmd == "list_guest_by_floor":
+            print("list_guest_by_floor")
+            return
+            
+        elif cmd == "checkout_guest_by_floor":
+            print("checkout_guest_by_floor")
+            return
+            
+        elif cmd == "book_by_floor":
+            print("book_by_floor")
+            return
+        
+        i += 1
+                    
+                
+                
 
 file = readFile()
 command = getCommand(file)
