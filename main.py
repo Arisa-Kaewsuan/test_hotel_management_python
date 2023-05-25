e7dbb33233dad5ad9a2fa0cb0a4680c6f191d90e
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-05-25 22:41:37 +0700

    store data read from file in "List"

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def getData():
-    data = open("input.txt", "r")
-    print(data.read())
-
-getData()
+def readFile():
+    # STEP 1 : read file
+    file = open("input.txt", "r")
+    # TEST  STEP 1
+    # print(file.read())
+    # print(file.readline())
+    
+    
+    # STEP 2 : store data read from file in "List"
+    for line in file:
+        file_list = line.split()
+        # TEST  STEP 2
+        # print(type(file_list))
+        # print(file_list)
+    
+    
+        
+readFile()
