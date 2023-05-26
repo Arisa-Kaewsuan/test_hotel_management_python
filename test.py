a336f03f5fb665aeb6b99478e7f9eb44293d06df
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-05-26 15:09:29 +0700

    store room_book_number

--- test.py
+++ test.py
@@ -32,19 +32,30 @@ def getCommand(file):
 def guestList(file):
     command = []
     guest = []
+    room_booked = set()
+    keygard_number = 0
+    j = 0
     for line in file.splitlines():
-        list = line.splitlines()
-        for i in list:  # String
-            arr = i.split()  # list -line
-            if arr[0] == "book":
-                #print("add data")
-                guest.append(arr)
+        list = line.splitlines() # list
+        for i in list:  # i = String : "book 203 Thor 32", ...
+            arr = i.split()  # list -line : [['book', '203', 'Thor', '32'], ...]
+            if arr[0] == "book" :
+                # print(arr[1])
+                room_booked.add(arr[1])
+        #         # print("add data")
+        #         # keygard_number += 1
+        #         # guest.append(arr)
+        #         # guest[j].append(keygard_number)  # add keycard_number : [['book', '203', 'Thor', '32', 1], ...]
+        #         # j += 1  
+    print(room_booked)
     return guest
 
 
 file = readFile()
 line = readLine(file)
 command = getCommand(file)
+# print(command)
 guest = guestList(file)
-#print(guest)
+# print(guest)
+
 
