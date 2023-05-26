1902d07f52722acc37562ddb74d3c02ad3980bf9
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-05-26 20:33:19 +0700

    class&inheritance OOP

--- createHotel.py
+++ createHotel.py
@@ -1,10 +1,17 @@
-
-def createHotel():
-    floor = 2
-    room = 3
-    message = print(f"Hotel created with {floor} floor(s), {room} room(s) per floor.")
-    return message
+import checkAllCase as ck
 
 
-createHotel()
+class createHotel(ck.checkAllCase):
+    def __init__(self,path):
+        super().__init__(path)
+        
+    # def createHotel():
+    #     floor = 2
+    #     room = 3
+    #     message = print(f"Hotel created with {floor} floor(s), {room} room(s) per floor.")
+    #     return message
 
+
+path = 'input.txt'
+x = createHotel(path)
+print(x.command())
