b1ffac71bef78ccc9a1c2816134da13642589d31
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-05-26 13:17:58 +0700

    ceate hotel success

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# input  : pathfile
+# output : str file
 def readFile():
     file = open("input.txt", "r")
     return file.read()  # String
 
-
+# input  : str file
+# output : line list
 def readLine(file):
     arr = []
     for line in file.splitlines():
@@ -11,8 +14,9 @@ def readLine(file):
             arr.append(i.split())
     return arr
 
-
-def getCommand(file):
+# input  : str file
+# output : line list [0]
+def getCommand(file): 
     command = []
     for line in file.splitlines():
         list = line.splitlines()
@@ -21,33 +25,64 @@ def getCommand(file):
             command.append(arr[0])
     return command
 
+# input  : line list / i (cmd)
+# output : print messege creat success
+def createHotel(line,i):
+    floor = line[i][1]
+    room = line[i][2]
+    return print(f"Hotel created with {floor} floor(s), {room} room(s) per floor.")
+    
+# input  : line list / i (cmd)
+# output : print messege book success or not
+def bookRoom(line,i):
+    room_list = []
+    room_input = room_list.append(line[i][1])
+    print(room_input)
+    book_name = line[i][2]
+    keycard_number = 1
+    
+    j = 0
+    while True:
+        if room_input == line[i][j]:
+            print("cannot book")
+            break
+        else:
+            room_list.append(line[i][1])
+            print("book succes")
+            break
+    return 
 
-def checkCase(command,line):
-    # print(len(command))
+
+#print(f"Room {room_number} is booked by {book_name} with keycard number {keycard_number}.")
+
+def list_available_rooms():
+    return
+
+def checkCase(command, line):
     i = 0
     while i < len(command):
-        cmd = command[i]
-        
+        cmd = command[i] 
+
+        # create_hotel
         if cmd == "create_hotel":
-            print("Hotel created with " + line[i][i+1] + " floor(s), " + line[i][i+2] + " room(s) per floor.")
-            return
-           
+           createHotel(line,i)
+        
+        # booking room
         elif cmd == "book":
-            print("book")
-            return
-            
+            bookRoom(line,i)
+                
         elif cmd == "list_available_rooms":
             print("list_available_rooms")
             return
-           
+
         elif cmd == "checkout":
             print("checkout")
             return
-            
+
         elif cmd == "list_guest":
             print("list_guest")
             return
-        
+
         elif cmd == "get_guest_in_room":
             print("get_guest_in_room")
             return
@@ -55,25 +90,24 @@ def checkCase(command,line):
         elif cmd == "list_guest_by_age":
             print("list_guest_by_age ")
             return
-        
+
         elif cmd == "list_guest_by_floor":
             print("list_guest_by_floor")
             return
-            
+
         elif cmd == "checkout_guest_by_floor":
             print("checkout_guest_by_floor")
             return
-            
+
         elif cmd == "book_by_floor":
             print("book_by_floor")
             return
-        
+
         i += 1
-                    
-                
-                
+
 
 file = readFile()
 line = readLine(file)
 command = getCommand(file)
-checkCase(command,line)
+checkCase(command, line)
+
